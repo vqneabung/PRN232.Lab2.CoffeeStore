b6a5eed0225ad39df7959f1764e825379616f092
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-10-01 21:17:48 +0700

    Add Common submodule and GenericRepository class
    Updated .gitignore to exclude sensitive files like .env and
    FodyWeavers.xsd. Added the "Common" submodule and updated
    it to a specific commit. Removed hardcoded connection string
    from CoffeeStoreDB2Context for improved security.
    
    Introduced GenericRepositoryWithContext class to provide a
    generic, reusable data access layer with support for filtering,
    sorting, pagination, and eager loading. Updated solution file
    to include the "Common" project and new folder structure.

--- PRN232.Lab2.CoffeeStore.Repository/Data/CoffeeStoreDB2Context.cs
+++ PRN232.Lab2.CoffeeStore.Repository/Data/CoffeeStoreDB2Context.cs
@@ -28,9 +28,6 @@ public partial class CoffeeStoreDB2Context : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=\"localhost, 1435\";Initial Catalog=CoffeeStoreDB2;User ID=sa;Password=Aa@12345");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
