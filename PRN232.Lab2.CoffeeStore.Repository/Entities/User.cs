7e568a4cad6a0005ddd49fb5f67a73edbb0b0eb1
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-10-02 08:21:58 +0700

    Add JWT, OpenAPI, and User entity; update references
    Added JWT and OpenAPI package references to the API project.
    Introduced a new `User` entity inheriting from `IdentityUser<Guid>`
    with additional properties like `FullName`, `RefreshToken`, and
    `Orders`. Updated project references in `Repositories` and
    `Services` projects to use `Common.csproj`. Adjusted DTO
    namespace imports in `IProductService.cs`. Added a new project
    to the solution file.

---
+++ PRN232.Lab2.CoffeeStore.Repository/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232.Lab2.CoffeeStore.Repositories.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime RefreshTokenExpiryTime { get; set; } = DateTime.UtcNow.AddDays(7);
+        public string? DeviceToken { get; set; }
+        public bool IsActive { get; set; } = true;
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
